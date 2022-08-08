0c61e66b6e793e43f1a09280db33560ed22f41c3
Ryan Wolfe <ryan.wolfe@strongmind.com>
2022-08-08 03:53:44 -0700

    Smaller header when not at top of window

--- app/javascript/controllers/header_controller.js
+++ app/javascript/controllers/header_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["navbar", "mobileDropdown"]
+  static targets = ["navbar", "mobileDropdown", "nav"]
 
   connect() {
     console.log(this.hasNavbarTarget)
@@ -16,6 +16,15 @@ export default class extends Controller {
     } else {
       this.navbarTarget.classList.remove('top-[-100px]')
       this.navbarTarget.classList.add('top-0')
+    }
+
+    if (scrollTop > 30) {
+      this.navbarTarget.classList.add('shadow-lg')
+      this.navbarTarget.classList.remove('py-2')
+
+    } else {
+      this.navbarTarget.classList.remove('shadow-lg')
+      this.navbarTarget.classList.add('py-2')
     }
     this.lastScrollTop = scrollTop
   }

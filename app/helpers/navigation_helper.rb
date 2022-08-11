968fcb0073ba9c6583e3bb7995813f0cb0ba1e34
Ryan Wolfe <ryan.wolfe@strongmind.com>
2022-08-11 00:35:30 -0700

    Basic styling for contact form view

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -6,7 +6,7 @@ module NavigationHelper
       { title: 'Home', url: root_path(anchor: 'home'), active: current_page?(root_path) },
       { title: 'About', url: root_path(anchor: 'about'), active: false },
       { title: 'Projects', url: root_path(anchor: 'projects'), active: false },
-      { title: 'Contact', url: root_path(anchor: 'contact'), active: false }
+      { title: 'Contact', url: contact_path, active: controller_name == 'contacts' }
     ]
   end
 end

e45920ae9b356f0df62740637eae23b67003c3c9
Ryan Wolfe <ryan.wolfe@strongmind.com>
2022-08-16 18:18:07 -0700

    Add background color to header on scroll instead of hiding

--- app/javascript/controllers/header_controller.js
+++ app/javascript/controllers/header_controller.js
@@ -4,26 +4,27 @@ export default class extends Controller {
   static targets = ["navbar", "mobileDropdown", "nav"]
 
   connect() {
-    window.addEventListener('scroll', (e) => this.handleScroll(e))
+    this.handleScroll()
+    window.addEventListener('scroll', (e) => this.handleScroll())
   }
 
-  handleScroll(e) {
+  handleScroll() {
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop
-    if ((scrollTop > this.lastScrollTop) && !this.mobileDropdownTarget.classList.contains('open')) {
-      this.navbarTarget.classList.remove('top-0')
-      this.navbarTarget.classList.add('top-[-100px]')
-    } else {
-      this.navbarTarget.classList.remove('top-[-100px]')
-      this.navbarTarget.classList.add('top-0')
-    }
+    // if ((scrollTop > this.lastScrollTop) && !this.mobileDropdownTarget.classList.contains('open')) {
+    //   this.navbarTarget.classList.remove('top-0')
+    //   this.navbarTarget.classList.add('top-[-100px]')
+    // } else {
+    //   this.navbarTarget.classList.remove('top-[-100px]')
+    //   this.navbarTarget.classList.add('top-0')
+    // }
 
     if (scrollTop > 30) {
-      this.navbarTarget.classList.add('shadow-lg')
-      this.navbarTarget.classList.remove('py-2')
+      this.navbarTarget.classList.add('shadow-lg', 'bg-gradient-to-r', 'from-primary/[.2]', 'via-primary/[.15]', 'to-bg-base')
+      this.navbarTarget.classList.remove('py-2', 'bg-transparent')
 
     } else {
-      this.navbarTarget.classList.remove('shadow-lg')
-      this.navbarTarget.classList.add('py-2')
+      this.navbarTarget.classList.remove('shadow-lg', 'bg-gradient-to-r', 'from-primary/[.2]', 'via-primary/[.15]', 'to-bg-base')
+      this.navbarTarget.classList.add('py-2', 'bg-transparent')
     }
     this.lastScrollTop = scrollTop
   }
